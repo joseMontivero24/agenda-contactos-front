11787395154121442cf8043a4f9e90c71f1c4594
Jose Montivero <chipimontivero2019@gmail.com>
2026-06-19 09:34:05 -0300

    feat: agregando un componente Sidebar.tsx en la pagina principal ContactoPages.tsx

--- src/componets/Sidebar.tsx
+++ src/componets/Sidebar.tsx
@@ -1,6 +1,54 @@
+import { FaAddressBook, FaUpload, FaUser, FaUserPlus } from "react-icons/fa"
 
 export const Sidebar = () => {
+  
+  
   return (
-    <div>Sidebar</div>
+    
+    <aside className="w-64 bg-slate-900 text-white flex flex-col h-screen">
+        {/* Logo */}
+        <div className="p-6 border-b border-slate-700">
+          <div className="flex items-center gap-3">
+            <FaAddressBook size={32}/>
+            <div>
+              <h1 className="font-bold text-xl">Agenda</h1>
+              <p>Contactos</p>
+            </div>
+          </div>
+        </div>
+
+
+        {/* Menu */}
+        <nav className="flex-1 p-4">
+          <ul className="space-y-3">
+            <li>
+              <button className="w-full flex items-center gap-3 bg-blue-600 px-4 py-3 rounded-lg">
+                <FaUser/>
+                Contactos
+              </button>
+            </li>
+
+            <li>
+              <button className="w-full flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-slate-800">
+                <FaUserPlus/>
+                Agenda Contacto
+              </button>
+            </li>
+
+            <li>
+              <button className="w-full flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-slate-800">
+                <FaUpload />
+                Importar contactos
+              </button>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Footer */}
+        <footer className="p-4 border-t border-slate-700">
+           © 2026 Agenda Contactos
+        </footer>
+
+    </aside>
   )
 }
--- src/pages/ContactoPages.tsx
+++ src/pages/ContactoPages.tsx
@@ -1,8 +1,16 @@
+// Pagina principal
 
+import { Sidebar } from "../componets/Sidebar"
 
 
 export const ContactoPages = () => {
   return (
-    <div className="mt-2">ContactoPages</div>
+    <div className="flex">
+        <Sidebar />
+
+        <main className="flex-1 p-8">
+            <h1 className="text-4xl font-bold">Contactos</h1>
+        </main>
+    </div>
   )
 }
